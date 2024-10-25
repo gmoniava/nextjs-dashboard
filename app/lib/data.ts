42b7285109be65998378a582fe42a5d97be5ced1
gmoniava <moniava.g@gmail.com>
2024-10-25 21:57:10 +0400

    add search and pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,7 +12,7 @@ import { formatCurrency } from "./utils";
 export async function fetchRevenue() {
   try {
     // Artificial delay
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -25,7 +25,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
